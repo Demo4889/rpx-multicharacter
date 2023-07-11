ed60adc10b40f6e8f53410ff09fa80ef9eee9c8b
Zer0Cool0 <njlewis0093@gmail.com>
2023-07-10 22:39:58 -0400

    Update client.lua
    - Fixed further logic with female characters on the character select screen
    - Added handler for deleting a single ped on the screen instead of all.

--- modules/peds/client.lua
+++ modules/peds/client.lua
@@ -6,13 +6,19 @@
 Peds = {}
 Peds.PedList = {}
 
-Peds.SpawnPed = function(position)
-    local coords = Config.CharLocations[CurrentScene][position]
-    RequestModel(GetHashKey("mp_male"))
-    while not HasModelLoaded(GetHashKey("mp_male")) do
-        Wait(1)    
+Peds.SpawnPed = function(position, gender)
+    local Model
+    if tonumber(gender) == 0 then
+        Model = 'mp_female'
+    else
+        Model = 'mp_male'
     end
-    local ped = CreatePed(GetHashKey("mp_male"), coords.x, coords.y, coords.z - 1.0, coords.w, false, true)
+    local coords = Config.CharLocations[CurrentScene][position]
+    RequestModel(GetHashKey(Model))
+    while not HasModelLoaded(GetHashKey(Model)) do
+        Wait(1)
+    end
+    local ped = CreatePed(GetHashKey(Model), coords.x, coords.y, coords.z - 1.0, coords.w, false, true)
     while not DoesEntityExist(ped) do
         Wait(1)
     end
@@ -24,7 +30,7 @@ Peds.SpawnPed = function(position)
     SetEntityInvincible(ped, true)
     SetBlockingOfNonTemporaryEvents(ped, true)
     SetEntityCanBeDamagedByRelationshipGroup(ped, false, GetHashKey("PLAYER"))
-    SetModelAsNoLongerNeeded(GetHashKey("mp_male"))
+    SetModelAsNoLongerNeeded(GetHashKey(Model))
     Peds.PedList[position] = ped
     return Peds.PedList[position]
 end
@@ -33,4 +39,8 @@ Peds.DestroyPeds = function()
     for position,ped in pairs(Peds.PedList) do
         DeleteEntity(ped)
     end
+end
+
+Peds.DestroyPed = function(position)
+    DeleteEntity(Peds.PedList[position])
 end
